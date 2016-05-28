54bf52e6b31fb4f9da732c038f67fd4611119a9c
Stijn <stijn.vanhaute@gmail.com>
2016-05-28 12:21:02 +0200

    finetune js datepick

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,11 @@ $(function(){
   $('.button-collapse').sideNav();
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
+    selectYears: 15, // Creates a dropdown of 15 years to control year
+    onSet: function( arg ){
+      if ( 'select' in arg ){ //prevent closing on selecting month/year
+          this.close();
+      }
+    }
   });
 });
-
